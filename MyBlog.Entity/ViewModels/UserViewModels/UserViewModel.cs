032de704407d821a93c46d409958d762d25ce995
Caner Bayraktar <cortactiondlx@gmail.com>
2024-04-19 13:39:03 +0300

    Listing of all users has been done

---
+++ MyBlog.Entity/ViewModels/UserViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MyBlog.Entity.ViewModels.UserViewModels;
+public class UserViewModel
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public bool EmailConfirmed { get; set; }
+    public string PhoneNumber { get; set; }
+    public int AccessFailed { get; set; }
+    public string Role { get; set; }
+}
---
+++ MyBlog.Service/AutoMapper/Users/UsersProfile.cs
@@ -0,0 +1,10 @@
+﻿using MyBlog.Entity.ViewModels.UserViewModels;
+
+namespace MyBlog.Service.AutoMapper.Users;
+public class UsersProfile: Profile
+{
+    public UsersProfile()
+    {
+        CreateMap<AppUser, UserViewModel>().ReverseMap();
+    }
+}
