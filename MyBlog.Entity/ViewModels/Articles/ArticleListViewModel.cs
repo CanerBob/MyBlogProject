1fcb80221ed843132cf90c169ef1a0b3a988aa11
Caner Bayraktar <cortactiondlx@gmail.com>
2024-05-03 14:19:42 +0300

    A method was written on the service side for pagination.

---
+++ MyBlog.Entity/ViewModels/Articles/ArticleListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyBlog.Entity.Entities;
+
+namespace MyBlog.Entity.ViewModels.Articles;
+public class ArticleListViewModel
+{
+    public IList<Article> Articles { get; set; }
+    public Guid? CategoryId { get; set; }
+    public virtual int CurrentPage { get; set; } = 1;
+    public virtual int PageSize { get; set; } = 3;
+    public virtual int TotalCount { get; set; }
+    public virtual int TotalPage => (int)Math.Ceiling(decimal.Divide(TotalCount , PageSize));
+    public virtual bool ShowPrevius => CurrentPage > 1;
+    public virtual bool ShowNext => CurrentPage < TotalPage;
+    public virtual bool IsAscending { get; set; } = false;
+}
--- MyBlog.Service/Services/Concrete/ArticleService.cs
+++ MyBlog.Service/Services/Concrete/ArticleService.cs
@@ -1,113 +1,111 @@
-﻿using MyBlog.Service.Extensions;
-using MyBlog.Service.Helpers.Images;
-using System.Security.Claims;
-
-namespace MyBlog.Service.Services.Concrete;
+﻿namespace MyBlog.Service.Services.Concrete;
 public class ArticleService : IArticleService
 {
-    private readonly IUnitOfWork unitOfWork;
-    private readonly IMapper mapper;
-    private readonly IHttpContextAccessor httpContextAccessor;
-    private readonly IImageHelper imageHelper;
-    private readonly ClaimsPrincipal _user;
+	private readonly IUnitOfWork unitOfWork;
+	private readonly IMapper mapper;
+	private readonly IHttpContextAccessor httpContextAccessor;
+	private readonly IImageHelper imageHelper;
+	private readonly ClaimsPrincipal _user;
+	public ArticleService(IUnitOfWork unitOfWork, IMapper mapper, IHttpContextAccessor httpContextAccessor, IImageHelper imageHelper)
+	{
+		this.unitOfWork = unitOfWork;
+		this.mapper = mapper;
+		this.httpContextAccessor = httpContextAccessor;
+		_user = httpContextAccessor.HttpContext.User;
+		this.imageHelper = imageHelper;
+	}
+	public async Task<ArticleListViewModel> GetAllByPagingAsync(Guid? categoryId, int currentPage = 1, int pageSize = 3, bool isAscending = false)
+	{
+		pageSize = pageSize > 20 ? 20 : pageSize;
 
-    public ArticleService(IUnitOfWork unitOfWork, IMapper mapper, IHttpContextAccessor httpContextAccessor, IImageHelper imageHelper)
-    {
-        this.unitOfWork = unitOfWork;
-        this.mapper = mapper;
-        this.httpContextAccessor = httpContextAccessor;
-        _user = httpContextAccessor.HttpContext.User;
-        this.imageHelper = imageHelper;
-    }
-    public async Task<List<ArticleViewModel>> GetAllArticlesWithCategoryNonDeletedAsync()
-    {
+		var articles = categoryId == null
+			? await unitOfWork.GetRepository<Article>().GetAllAsync(x => !x.IsDeleted, x => x.Category, c => c.Image)
+			: await unitOfWork.GetRepository<Article>().GetAllAsync(x => x.CategoryId == categoryId && !x.IsDeleted, a => a.Category, c => c.Image);
 
-        var articles = await unitOfWork.GetRepository<Article>().GetAllAsync(x => !x.IsDeleted, x => x.Category);
-        var map = mapper.Map<List<ArticleViewModel>>(articles);
-        return map;
-    }
-    public async Task<ArticleViewModel> GetArticleWithCategoryNonDeletedAsync(Guid articleId)
-    {
+		var sortedArticles = isAscending
+			? articles.OrderBy(x => x.CreatedDate).Skip((currentPage - 1) * pageSize).Take(pageSize).ToList()
+			: articles.OrderByDescending(x => x.CreatedDate).Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
 
-        var article = await unitOfWork.GetRepository<Article>().GetAsync(x => !x.IsDeleted && x.Id == articleId, x => x.Category, x => x.Image);
-        var map = mapper.Map<ArticleViewModel>(article);
-        return map;
-    }
-
-    public async Task CreateArticleAsync(ArticleAddViewModel model)
-    {
-        //var userId = Guid.Parse("EEB3A51F-5450-4C4F-8562-550D2DEAE903");
-        //var imageId = Guid.Parse("F406068B-EC45-4D22-B22E-084B4705D8B5");
-        var userId = _user.GetLoggedInUserId();
-        var userName = _user.GetLoggedInUserName();
-
-        var imageUpload = await imageHelper.Upload(model.Title,model.Photos,ImageType.Post);
-        Image image = new(imageUpload.FullName, model.Photos.ContentType, userName);
-        await unitOfWork.GetRepository<Image>().AddAsync(image);
-
-        var article = new Article(model.Title, model.Content, userId, userName, model.CategoryId, image.Id);
-        await unitOfWork.GetRepository<Article>().AddAsync(article);
-        await unitOfWork.SaveAsync();
-    }
-    public async Task<string> UpdateArticleAsync(ArticleUpdateViewModel articleUpdateVm)
-    {
-        var userName = _user.GetLoggedInUserName();
-        var article = await unitOfWork.GetRepository<Article>().GetAsync(x => !x.IsDeleted && x.Id == articleUpdateVm.Id, x => x.Category, x => x.Image);
-
-        if (articleUpdateVm.Photo != null)
-        {
-            imageHelper.Delete(article.Image.FileName);
-            var imageUpload = await imageHelper.Upload(articleUpdateVm.Title,articleUpdateVm.Photo,ImageType.Post);
-            Image image = new(imageUpload.FullName,articleUpdateVm.Photo.ContentType,userName);
-            await unitOfWork.GetRepository<Image>().AddAsync(image);
-
-            article.ImageId = image.Id;
-
-
-        }
-
-
-
-        article.Title = articleUpdateVm.Title;
-        article.Content = articleUpdateVm.Content;
-        article.CategoryId = articleUpdateVm.CategoryId;
-        article.ModifiesDate = DateTime.Now;
-        article.ModifiedBy = userName;
-
-
-
-        await unitOfWork.GetRepository<Article>().UpdateAsync(article);
-        await unitOfWork.SaveAsync();
-        return article.Title;
-    }
-    public async Task<string> SafeDeleteArticleAsync(Guid articleId)
-    {
-        var userName = _user.GetLoggedInUserName();
-        var article = await unitOfWork.GetRepository<Article>().GetByGuidAsync(articleId);
-        article.IsDeleted = true;
-        article.DeletedDate = DateTime.Now;
-        article.DeletedBy = userName;
-        await unitOfWork.GetRepository<Article>().UpdateAsync(article);
-        await unitOfWork.SaveAsync();
-        return article.Title;
-    }
-
-    public async Task<List<ArticleViewModel>> GetAllArticlesWithCategoryDeletedAsync()
-    {
-        var article = await unitOfWork.GetRepository<Article>().GetAllAsync(x => x.IsDeleted, x=>x.Category);
-        var map = mapper.Map<List<ArticleViewModel>>(article);
-        return map;
-    }
-
-    public async Task<string> UndoDeleteArticleAsync(Guid articleId)
-    {
-        var userName = _user.GetLoggedInUserName();
-        var article = await unitOfWork.GetRepository<Article>().GetByGuidAsync(articleId);
-        article.IsDeleted = false;
-        article.DeletedDate = null;
-        article.DeletedBy = null;
-        await unitOfWork.GetRepository<Article>().UpdateAsync(article);
-        await unitOfWork.SaveAsync();
-        return article.Title;
-    }
+		return new ArticleListViewModel
+		{
+			Articles = sortedArticles,
+			CategoryId = categoryId == null ? null : categoryId.Value,
+			CurrentPage = currentPage,
+			PageSize = pageSize,
+			TotalCount = articles.Count,
+			IsAscending = isAscending
+		};
+	}
+	public async Task<List<ArticleViewModel>> GetAllArticlesWithCategoryNonDeletedAsync()
+	{
+		var articles = await unitOfWork.GetRepository<Article>().GetAllAsync(x => !x.IsDeleted, x => x.Category);
+		var map = mapper.Map<List<ArticleViewModel>>(articles);
+		return map;
+	}
+	public async Task<ArticleViewModel> GetArticleWithCategoryNonDeletedAsync(Guid articleId)
+	{
+		var article = await unitOfWork.GetRepository<Article>().GetAsync(x => !x.IsDeleted && x.Id == articleId, x => x.Category, x => x.Image);
+		var map = mapper.Map<ArticleViewModel>(article);
+		return map;
+	}
+	public async Task CreateArticleAsync(ArticleAddViewModel model)
+	{
+		var userId = _user.GetLoggedInUserId();
+		var userName = _user.GetLoggedInUserName();
+		var imageUpload = await imageHelper.Upload(model.Title, model.Photos, ImageType.Post);
+		Image image = new(imageUpload.FullName, model.Photos.ContentType, userName);
+		await unitOfWork.GetRepository<Image>().AddAsync(image);
+		var article = new Article(model.Title, model.Content, userId, userName, model.CategoryId, image.Id);
+		await unitOfWork.GetRepository<Article>().AddAsync(article);
+		await unitOfWork.SaveAsync();
+	}
+	public async Task<string> UpdateArticleAsync(ArticleUpdateViewModel articleUpdateVm)
+	{
+		var userName = _user.GetLoggedInUserName();
+		var article = await unitOfWork.GetRepository<Article>().GetAsync(x => !x.IsDeleted && x.Id == articleUpdateVm.Id, x => x.Category, x => x.Image);
+		if (articleUpdateVm.Photo != null)
+		{
+			imageHelper.Delete(article.Image.FileName);
+			var imageUpload = await imageHelper.Upload(articleUpdateVm.Title, articleUpdateVm.Photo, ImageType.Post);
+			Image image = new(imageUpload.FullName, articleUpdateVm.Photo.ContentType, userName);
+			await unitOfWork.GetRepository<Image>().AddAsync(image);
+			article.ImageId = image.Id;
+		}
+		article.Title = articleUpdateVm.Title;
+		article.Content = articleUpdateVm.Content;
+		article.CategoryId = articleUpdateVm.CategoryId;
+		article.ModifiesDate = DateTime.Now;
+		article.ModifiedBy = userName;
+		await unitOfWork.GetRepository<Article>().UpdateAsync(article);
+		await unitOfWork.SaveAsync();
+		return article.Title;
+	}
+	public async Task<string> SafeDeleteArticleAsync(Guid articleId)
+	{
+		var userName = _user.GetLoggedInUserName();
+		var article = await unitOfWork.GetRepository<Article>().GetByGuidAsync(articleId);
+		article.IsDeleted = true;
+		article.DeletedDate = DateTime.Now;
+		article.DeletedBy = userName;
+		await unitOfWork.GetRepository<Article>().UpdateAsync(article);
+		await unitOfWork.SaveAsync();
+		return article.Title;
+	}
+	public async Task<List<ArticleViewModel>> GetAllArticlesWithCategoryDeletedAsync()
+	{
+		var article = await unitOfWork.GetRepository<Article>().GetAllAsync(x => x.IsDeleted, x => x.Category);
+		var map = mapper.Map<List<ArticleViewModel>>(article);
+		return map;
+	}
+	public async Task<string> UndoDeleteArticleAsync(Guid articleId)
+	{
+		var userName = _user.GetLoggedInUserName();
+		var article = await unitOfWork.GetRepository<Article>().GetByGuidAsync(articleId);
+		article.IsDeleted = false;
+		article.DeletedDate = null;
+		article.DeletedBy = null;
+		await unitOfWork.GetRepository<Article>().UpdateAsync(article);
+		await unitOfWork.SaveAsync();
+		return article.Title;
+	}
 }
